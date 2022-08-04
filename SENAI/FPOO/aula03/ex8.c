e41d726ebe19b11acf3877bc58f6e46dc9e9535d
Isaac Cordeiro Gonçalves <110203041+isaacgoncod@users.noreply.github.com>
2022-08-04 09:45:15 -0300

    presencial

--- SENAI/FPOO/aula03/ex8.c
+++ SENAI/FPOO/aula03/ex8.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<locale.h>
 #include<string.h>
-#include<math.h>
 
 /* Desenvolva um programa que leia o raio (cm)
  e a altura (cm) de um cilindro.
@@ -11,5 +10,16 @@
 void main() {
 	setlocale(LC_ALL, "Portuguese");
 	
+	float raio, area, altura, volume;
+	
+	printf("Digíte o valor do raio(cm2): ");
+	scanf("%f", &raio);
+	printf("Digíte o valor da altura(cm2): ");
+	scanf("%f", &altura);
+	
+	area = (raio * raio * 3.1415) * 2; 
+	volume = (raio * raio * 3.1415) * altura;
+	
+	printf("A área do cilindro é de %.2f(cm2) e o volume é %.2f(cm3).", area, volume);
 	
 }
