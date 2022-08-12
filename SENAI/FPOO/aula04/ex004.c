61eff0e796593ed2379f190d7efbe0f44b2bc791
Isaac Cordeiro Gonçalves <110203041+isaacgoncod@users.noreply.github.com>
2022-08-11 22:14:22 -0300

    casa

--- SENAI/FPOO/aula04/ex004.c
+++ SENAI/FPOO/aula04/ex004.c
@@ -14,12 +14,12 @@ void main() {
 	printf("Digíte o valor de c : ");	
 	scanf("%f", &ladoc);
 	
-	if(ladoa == ladob == ladoc ){
+	if(ladoa == ladob == ladoc){
 		printf("Todos os lados são iguais, ou seja, o triângulo é EQUILÁTERO.");
-	}else if(ladoa == ladob =:!= ladoc){
-		printf("Todos os lados são iguais, ou seja, o triângulo é ISÓSCELES.");
-	}else if(ladoa != ladob != ladoc){
-		printf("Todos os lados são iguais, ou seja, o triângulo é ESCALENO.");
+	}else if(ladob == ladoc){
+		printf("Todos os lados são iguais, ou seja, o triângulo é EQUILÁTERO.");
+	}else if(ladoc != ladoa){
+		printf("Somente dois lados são iguais, ou seja, o triângulo é ESCALENO.");
 	}else{
 		printf("\nAcabou");
 	}
