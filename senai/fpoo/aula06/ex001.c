60e57f89528caa940947186897041aa87480267c
isaacgoncod <isaacgoncod@gmail.com>
2022-09-04 16:32:34 -0300

    edit readme

--- senai/fpoo/aula06/ex001.c
+++ senai/fpoo/aula06/ex001.c
@@ -5,23 +5,23 @@ void main(){
 	setlocale(LC_ALL, "Portuguese");
 
 	int numsa [5], numsb [5], i;
-	
+
 	for(i = 0; i < 5; i++){
-	printf("Digíte o %dº valor do primeiro vetor : ", i + 1);
+	printf("DigÃ­te o %dÂº valor do primeiro vetor : ", i + 1);
 	scanf("%d", &numsa[i]);
 	}
-	
+
 	printf("--------------------------------------------------\n");
-	
+
 	for(i = 0; i < 5; i++){
-	printf("Digíte o %dº valor do segundo vetor : ", i + 1);
+	printf("DigÃ­te o %dÂº valor do segundo vetor : ", i + 1);
 	scanf("%d", &numsb[i]);
 	}
 	printf("--------------------------------------------------\n\n");
-	printf("A soma de %d com %d é de : %d\n",numsa[0], numsb[4], numsa[0] + numsb[4]);
-	printf("A soma de %d com %d é de : %d\n",numsa[1], numsb[3], numsa[1] + numsb[3]);
-	printf("A soma de %d com %d é de : %d\n",numsa[2], numsb[2], numsa[2] + numsb[2]);
-	printf("A soma de %d com %d é de : %d\n",numsa[3], numsb[1], numsa[3] + numsb[1]);
-	printf("A soma de %d com %d é de : %d\n",numsa[4], numsb[0], numsa[4] + numsb[0]);
-	
+	printf("A soma de %d com %d Ã© de : %d\n",numsa[0], numsb[4], numsa[0] + numsb[4]);
+	printf("A soma de %d com %d Ã© de : %d\n",numsa[1], numsb[3], numsa[1] + numsb[3]);
+	printf("A soma de %d com %d Ã© de : %d\n",numsa[2], numsb[2], numsa[2] + numsb[2]);
+	printf("A soma de %d com %d Ã© de : %d\n",numsa[3], numsb[1], numsa[3] + numsb[1]);
+	printf("A soma de %d com %d Ã© de : %d\n",numsa[4], numsb[0], numsa[4] + numsb[0]);
+
 }
