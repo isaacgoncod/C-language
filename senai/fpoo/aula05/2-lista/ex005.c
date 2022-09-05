6127ab09667085270072f3e79f2ebdef4e03cb4a
isaacgoncod <isaacgoncod@gmail.com>
2022-09-04 22:59:04 -0300

    edit lista2

--- senai/fpoo/aula05/2-lista/ex005.c
+++ senai/fpoo/aula05/2-lista/ex005.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <math.h>
 
-void main () {
+void main (int argc, char** argv) {
 	setlocale(LC_ALL, "Portuguese");
-		
-		int quantidade, i = 1;
+
+		float a, b, c, x1, x2, delta;
+
+ // Le os termos da equacao
+    printf("Digite o valor do termo a: ");
+    scanf("%f", &a);
+    printf("Digite o valor do termo b: ");
+    scanf("%f", &b);
+    printf("Digite o valor do termo c: ");
+    scanf("%f", &c);
+
+ // Calcula o valor de x1
+ delta = b*b - 4*a*c;
+ x1 = (-b + sqrt(delta)) / (2*a);
+ x2 = (-b - sqrt(delta)) / (2*a);
+
+ // Imprime os resultados
+ if(delta < 0) {
+    printf("A equacao nao possui raizes reais.n");
+ } else {
+    printf("O valor de x1: %.2fn", x1);
+    printf("O valor de x2: %.2fn", x2);
+ }
 
 }
